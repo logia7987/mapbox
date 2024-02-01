62d8faaf67f5b896cda986c18012824f98f57f04
ohgati <dhtmdduq99@naver.com>
2024-02-01 10:53:38 +0900

    2/1 파일 업로드 로딩 화면

--- src/main/java/com/transit/mapbox/common/service/ShapeFileService.java
+++ src/main/java/com/transit/mapbox/common/service/ShapeFileService.java
@@ -16,7 +16,7 @@ public class ShapeFileService {
         SimpleFeatureIterator featureIterator  = shpDataStore.getFeatureSource().getFeatures().features();
 
         while (featureIterator.hasNext()) {
-            SimpleFeature feature = featureIterator.next()    ;
+            SimpleFeature feature = featureIterator.next();
 
             // 공간 데이터 출력
             System.out.println("Geometry: " + feature.getDefaultGeometry());
